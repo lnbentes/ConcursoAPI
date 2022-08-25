77ad682c1617394f8773c2e03bbdc954fb089cad
lucas bentes <lucasbentes16@gmail.com>
2022-08-25 16:47:12 -0300

    Configurando o swagger e iniciando o controller

--- src/main/java/br/com/lnbentes/api/concurso/controller/QuestaoController.java
+++ src/main/java/br/com/lnbentes/api/concurso/controller/QuestaoController.java
@@ -1,4 +1,11 @@
 package br.com.lnbentes.api.concurso.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/questao")
+@Tag(name = "Questão", description = "Endpoints para gerenciar as questões")
 public class QuestaoController {
 }
